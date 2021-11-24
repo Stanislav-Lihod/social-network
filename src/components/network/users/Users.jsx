65041a02868eaf3fbd463df4e4b80998367ec1e0
Stanislav Lihodievski <grollii@mail.ru>
2021-11-25 00:10:47 +0300

    refactoring

--- src/components/network/users/Users.jsx
+++ src/components/network/users/Users.jsx
@@ -5,31 +5,40 @@ import * as axios from 'axios'
 import notLogo from "../../../img/users/notUser.png"
 
 // const Users = (props) => {
-//   const users = props.users.map(user => <User key={user.id} id={user.id} name={user.name} prof={user.prof} logo={user.logo} bg={user.bg} followed={user.followed} onFollowed={props.onFollowed}
+//   const getUsers = () => {
+//     if (props.users.length === 0) {
+//       axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+//         props.setUsers(response.data.items)
+//       })
+//     }
+//   }
+//   const users = props.users.map(user => <User key={user.id} id={user.id} name={user.name} prof={user.prof} logo={user.photos.small != null ? user.photos.small : notLogo} bg={user.bg} followed={user.followed} onFollowed={props.onFollowed}
 //     onUnfollowed={props.onUnfollowed} />)
 //   return (
 //     <div className={stl.users}>
+//       <button onClick={getUsers}></button>
 //       {users}
 //     </div>
 //   )
 // }
 
 class Users extends React.Component {
-
-  constructor(props) {
-    super(props)
+  getUsers = () => {
+    if (this.props.users.length === 0) {
       axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
         this.props.setUsers(response.data.items)
       })
+    }
   }
-
-  users = this.props.users.map(user => <User key={user.id} id={user.id} name={user.name} prof={user.prof}
-    logo={user.photos.small != null ? user.photos.small : notLogo} bg={user.bg} followed={user.followed}
-    onFollowed={this.props.onFollowed} onUnfollowed={this.props.onUnfollowed} />)
-  render() {
-    return <div className={stl.users}>
-      {this.users}
-    </div>
+  users = this.props.users.map(user => <User key={user.id} id={user.id} name={user.name} prof={user.prof} logo={user.photos.small != null ? user.photos.small : notLogo} bg={user.bg} followed={user.followed} onFollowed={this.props.onFollowed}
+          onUnfollowed={this.props.onUnfollowed} />)
+  render(){
+    return (
+      <div className={stl.users}>
+        <button onClick={this.getUsers}></button>
+        {this.users}
+      </div>
+    )
   }
 }
 
