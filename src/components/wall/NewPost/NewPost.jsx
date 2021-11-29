ae080cf6eb5cb85a22b4ed58beb0f71b7fb139bd
Stanislav Lihodievski <grollii@mail.ru>
2021-11-29 10:25:42 +0300

    add users profile

--- src/components/wall/NewPost/NewPost.jsx
+++ src/components/wall/NewPost/NewPost.jsx
@@ -1,11 +1,11 @@
 import React from "react"
 import stl from "./newpost.module.css"
 
+const newPost = React.createRef()
 
 const NewPost = (props) => {
-  const newPost = React.createRef()
 
-  const onAddNewPost = () => props.addNewPost()
+  const onAddNewPost = () => newPost.current.value.length > 0 && props.addNewPost()
   const updateValue = () => props.updateValue(newPost.current.value)
 
   return (
@@ -17,8 +17,7 @@ const NewPost = (props) => {
         ref={newPost} 
         onKeyDown={e => { if (e.key === 'Enter') onAddNewPost()}} 
         value={props.valueText} 
-        onChange={() => updateValue()}>
-      </textarea>
+        onChange={() => updateValue()}></textarea>
       <div className={stl.newPost__additions}>
         <div className={stl.newPost__prompt}>What’s on your mind?</div>
       </div>
--- src/components/wall/Post/Posts.jsx
+++ src/components/wall/Post/Posts.jsx
@@ -1,14 +1,11 @@
 import React from "react"
 import Post from "./Post"
 
-// const Posts = (props) => {
-//   const renderPost = props.posts.map(post => <Post name={post.name} prof={post.prof} like={post.likesValue} comment={post.commentValue} post={post.post} />)
-//   return (<div>{renderPost}</div>)
-// }
-
 class Posts extends React.Component {
-  renderPost = this.props.posts.map(post => <Post key={post.id} name={post.name} prof={post.prof} like={post.likesValue} comment={post.commentValue} post={post.post} />)
-  render() {return (<div>{this.renderPost}</div>)}
+  render() {
+    const renderPost = this.props.posts.map(post => <Post key={post.id} name={post.name} prof={post.prof} like={post.likesValue} comment={post.commentValue} post={post.post} />)
+    return (<div>{renderPost}</div>)
+  }
 }
 
 export default Posts
